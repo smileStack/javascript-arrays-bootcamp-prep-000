7e66ac491554b31a3eb426c45a05b4d47375cfce
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-05 22:41:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,5 +21,6 @@ function addElementToEndOfArray(array, element){
   return   array = [...array,element];;
 }
 function destructivelyAddElementToEndOfArray(array, element){
-  return array.push(element);
+  array.push(element);
+  return array;
 }
