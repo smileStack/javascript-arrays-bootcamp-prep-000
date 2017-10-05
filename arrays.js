ac88c0656bd8bb4b9542131fdc0aeeef1e068880
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-05 23:05:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,7 +42,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array;
 }
 function removeElementFromBeginningOfArray(array){
-array.slice(0);
+array.slice(1);
 return array;
 }
 
