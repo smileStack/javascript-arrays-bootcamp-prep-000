3c56ce1439590723c9dcee5901c7e4d485d74ad3
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-05 22:33:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars =["snickers", "hundred grand", "kitkat", "skittles"];
 
 chocolateBars = addElementToBeginningOfArray(chocolateBars, "element")
 console.log(chocolateBars);
-destructivelyAddElementToBeginningOfArray(chocolateBars, "two")
+chocolateBars =destructivelyAddElementToBeginningOfArray(chocolateBars, "two")
 console.log(chocolateBars);
 
 function addElementToBeginningOfArray(array, element){
