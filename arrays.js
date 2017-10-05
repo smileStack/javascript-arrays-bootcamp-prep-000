66439c3fdb2e01bc2e6ff166ebd68e80a79329ee
Ashley Martinez <ashley.carrasquel@gmail.com>
2017-10-05 22:21:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element){
   array.unshift(element);
   console.log(array);
 }
-
+console.log(array);
 function destructivelyAddElementToBeginningOfArray(array, element){
   array[0]=element;
   console.log(array);
